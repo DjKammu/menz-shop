21acca7dfbffab0a67eb2f806b0d0190fa958437
Kamlesh DjKammu <dhundharakamlesh@gmail.com>
2021-11-11 11:10:37 +0530

    WIP

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -9,8 +9,8 @@
         <div class="row">
           <div class="col-md-12 menz-col pb-0">
             <div class="row">
-              <h1 class="forgot-title">Are you having trouble signing in?</h1>
-                <p class="section-text mt-4">Please enter your Kundennummer to get started.</p>
+              <h1 class="forgot-title">Haben Sie Probleme beim Anmelden?</h1>
+                <p class="section-text mt-4">Bitte geben Sie Ihre Kundennummer ein, um loszulegen.</p>
                 @if (session('status'))
                       <div class="alert alert-success" role="alert">
                           {{ session('status') }}
@@ -33,13 +33,13 @@
 
 
                   <button type="submit" class="button" id="continueBtn" disabled>
-                        Reset Password
+                        Passwort zurücksetzen
                   </button>
                 </form>
-                <p class="section-text mt-2">A Password reset link will be sent to your registered email.</p>
+                <p class="section-text mt-2">Ein Link zum Zurücksetzen des Passworts wird an Ihre registrierte E-Mail gesendet.</p>
           </div>
           <div class="row mt-5">
-            <h1 class="forgot-title mb-4">Enter your registered Email to find your Kundennumer</h1>
+            <h1 class="forgot-title mb-4">Geben Sie Ihre registrierte E-Mail ein, um Ihre Kundennummer zu finden</h1>
             @if (session('status-kundennummer'))
                 <div class="alert alert-success" role="alert">
                     {{ session('status-kundennummer') }}
@@ -49,7 +49,7 @@
                         @csrf
                 <div class="form-element  mb-2">
                     <input class="form-textbox form-textbox-text @error('email') is-invalid @enderror" id="forgotidInput" type="text" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus />
-                      <span aria-hidden="true" class="form-label" id="forgotPassLabel">Email Address</span>   
+                      <span aria-hidden="true" class="form-label" id="forgotPassLabel">E-Mail-Adresse</span>   
 
                       @error('email')
                           <span class="invalid-feedback" role="alert">
@@ -59,10 +59,10 @@
 
                 </div>
                 <button type="submit" class="button" id="idcontinueBtn" disabled>
-                      Get Kundennumer
+                      Kundennummer abrufen
                 </button>
               </form>
-              <p class="section-text mt-2">Your Kundennumer will be sent to your registered email.</p>
+              <p class="section-text mt-2">Ihre Kundennummer wird an Ihre registrierte E-Mail gesendet.</p>
         </div>
         </div>
       </div>
